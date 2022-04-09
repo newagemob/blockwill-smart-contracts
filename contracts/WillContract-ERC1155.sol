8a7c27a163be0884e10b4806999f70667ca3173e
newagemob <isaiah.wright@tutanota.com>
2022-04-09 04:21:17 -0500

    basic ERC-1155 attempt

---
+++ contracts/WillContract-ERC1155.sol
@@ -0,0 +1,29 @@
+// each token minted represents a living will to be fulfilled. the token can either be redeemed through lack of timed verification on the senders part, or it can be redeemed when the user submits a request that receives no authentication.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract BlockwillToken is ERC1155, Ownable {
+    constructor() ERC1155("https://api.mysite.com/tokens/{id}") {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    // mint a single token
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
